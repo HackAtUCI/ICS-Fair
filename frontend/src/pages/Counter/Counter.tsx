af9e3d2b6377f157d2cfcff4c3bf14cac8aba321
NoNathan17 <nathanong1217@gmail.com>
2025-05-06 22:04:41 -0700

    implemented the counter page

--- frontend/src/pages/Counter/Counter.tsx
+++ frontend/src/pages/Counter/Counter.tsx
@@ -1,3 +1,38 @@
+import React, { useState } from 'react'
+import './Counter.css'
+
 export default function Counter() {
-  return <></>;
-}
+  const [bags, setBags] = useState(0)
+
+  const increment = () => setBags(bags + 1)
+
+  const decrement = () => {
+    if (bags > 0) setBags(bags - 1)
+
+  };
+
+  const reset = () => setBags(0)
+
+  return (
+    <div className="counter-container">
+    <img src="/cowboyanteater.png" alt="cowboy anteater image" className="counter-image"></img>
+      <div className="counter-card">
+        <div className="bag-count">
+          <h1>{bags}</h1>
+          <p>Number of Bags</p>
+        </div>
+      </div>
+      <div className="counter-controls">
+        <div className="counter-increments">
+          <button onClick={decrement} className="counter-button">
+            <img src="/decrement.svg" alt="decrement" width="37" height="6"></img>
+          </button>
+          <button onClick={increment} className="counter-button">
+          <img src="/increment.svg" alt="decrement" width="34" height="34"></img>
+          </button>
+        </div>
+        <button onClick={reset} className="reset-button">RESET</button>
+      </div>
+    </div>
+    )
+};
