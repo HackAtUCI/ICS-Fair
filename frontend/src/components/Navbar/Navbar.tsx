42a3962cba8efac8812d31ab13c0b2cf8f421df4
NoNathan17 <nathanong1217@gmail.com>
2025-05-09 17:37:39 -0700

    prettier styling

--- frontend/src/components/Navbar/Navbar.tsx
+++ frontend/src/components/Navbar/Navbar.tsx
@@ -1,22 +1,24 @@
-import { Link, useLocation } from "react-router-dom"
-import "./Navbar.css"
+import { Link, useLocation } from "react-router-dom";
+import "./Navbar.css";
 
-export default function Navbar () {
-    const location = useLocation()
+export default function Navbar() {
+  const location = useLocation();
 
-    let buttonText = ''
-    let nextRoute = ''
+  let buttonText = "";
+  let nextRoute = "";
 
-    if (location.pathname === '/') {
-        buttonText = 'WEBCAM'
-        nextRoute = '/webcam'
-    } else {
-        buttonText = 'COUNTER'
-        nextRoute = '/'
-    }
-    return (
-        <nav className="navbar">
-            <Link to={nextRoute} className="nav-button">{buttonText}</Link>
-        </nav>
-    )
+  if (location.pathname === "/") {
+    buttonText = "WEBCAM";
+    nextRoute = "/webcam";
+  } else {
+    buttonText = "COUNTER";
+    nextRoute = "/";
+  }
+  return (
+    <nav className="navbar">
+      <Link to={nextRoute} className="nav-button">
+        {buttonText}
+      </Link>
+    </nav>
+  );
 }
