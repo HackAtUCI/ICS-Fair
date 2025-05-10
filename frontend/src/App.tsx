85e1fe3ad1bd114a92d9ef329d424200b12e171d
NoNathan17 <nathanong1217@gmail.com>
2025-05-09 17:32:03 -0700

    implemented navbar and all proper routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,12 +2,14 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Counter from "./pages/Counter/Counter";
 import Webcam from "./pages/Webcam/Webcam";
 import Canvas from "./pages/Canvas/Canvas";
+import Navbar from "./components/Navbar/Navbar"
 
 import "./App.css";
 
 function App() {
   return (
     <BrowserRouter>
+    <Navbar />
       <Routes>
         <Route path="/" element={<Counter />} />
         <Route path="/webcam" element={<Webcam />} />
