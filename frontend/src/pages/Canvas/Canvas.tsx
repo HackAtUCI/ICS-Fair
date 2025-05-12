890f78a490f77557a67b43054e6903155f68b201
Rishi Murumkar <108751289+RichieRish05@users.noreply.github.com>
2025-05-11 22:44:07 -0700

    Updated styling and integrated capture button

--- frontend/src/pages/Canvas/Canvas.tsx
+++ frontend/src/pages/Canvas/Canvas.tsx
@@ -1,3 +1,15 @@
+import React from 'react';
+import { useLocation } from 'react-router-dom';
+import './Canvas.css'
+
+
 export default function Canvas() {
-  return <></>;
+  const location = useLocation();
+  const { photo } = location.state || {};
+
+  return (
+    <div>
+      {photo && <img src={photo} alt="Captured photo" className="photo"/>}
+    </div>
+  );
 }
--- frontend/src/pages/Webcam/Webcam.tsx
+++ frontend/src/pages/Webcam/Webcam.tsx
@@ -1,3 +1,66 @@
+import './Webcam.css';
+import { useEffect, useRef } from 'react';
+import { useNavigate } from 'react-router-dom';
+
 export default function Webcam() {
-  return <></>;
+  const videoRef = useRef<HTMLVideoElement>(null);
+  const navigate = useNavigate();
+
+  const getVideo = () => {
+    navigator.mediaDevices.getUserMedia({
+      video: {
+        width: { ideal: window.innerWidth },
+        height: { ideal: window.innerHeight }
+      }
+    })
+    .then((stream) => {
+      let video = videoRef.current;
+      video.srcObject = stream;
+      video.play();
+    })
+    .catch((err) => {
+      console.error(err);
+    })
+  }
+
+  const capturePhoto = () => {
+
+    // Draw the current frame on a canvas
+    const video = videoRef.current;
+    const canvas = document.createElement('canvas');
+    canvas.width = video.videoWidth;
+    canvas.height = video.videoHeight;
+    const context = canvas.getContext('2d');
+    context.drawImage(video, 0, 0, canvas.width, canvas.height);
+
+    // Convert to jpeg
+    const imageData = canvas.toDataURL('image/jpeg');
+    
+    // Navigate to Canvas page with the photo
+    navigate('/canvas', { state: { photo: imageData } });
+  }
+
+
+  useEffect(() => {
+    getVideo();
+  }, [videoRef])
+
+  return (
+    <>
+      <div>
+        <h1 className='header'>WANTED</h1>
+        <video className="webcam-video" ref={videoRef}></video>
+      </div>
+      <button 
+        onClick={capturePhoto}
+        className="capture-button"
+      >
+        <img 
+          src="/camera-icon.png" 
+          alt="Capture photo"
+          className="camera-icon"
+        />
+      </button>
+    </>
+  );
 }
