727a02fc1d1fe3202d05a50699d089b75b719ae6
Vishok Lakshmankumar <vishoklax@gmail.com>
2025-05-20 06:07:10 -0700

    refactor: simplified IconButton props and styling

--- frontend/src/components/IconButton/IconButton.tsx
+++ frontend/src/components/IconButton/IconButton.tsx
@@ -1,35 +1,15 @@
+import "./IconButton.css";
+
 interface IconButtonProps {
   src: string;
   onClick?: () => void;
   title?: string;
-  size?: number;
-  backgroundColor?: string;
 }
 
-export default function IconButton({
-  src,
-  onClick,
-  title,
-  size = 48,
-  backgroundColor = "#BDB199",
-}: IconButtonProps) {
+export default function IconButton({ src, onClick, title }: IconButtonProps) {
   return (
-    <button
-      onClick={onClick}
-      title={title}
-      style={{
-        width: size,
-        height: size,
-        borderRadius: "50%",
-        background: backgroundColor,
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        border: "none",
-        cursor: "pointer",
-      }}
-    >
-      <img src={src} alt="" style={{ width: size / 2, height: size / 2 }} />
+    <button className="icon-button" onClick={onClick} title={title}>
+      <img src={src} alt="" />
     </button>
   );
 }
